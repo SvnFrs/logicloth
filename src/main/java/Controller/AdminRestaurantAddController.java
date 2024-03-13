e6305105ce438f012d305c33ce01d76109904d99
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2024-03-13 14:01:09 +0700

    Add controllers.

---
+++ src/main/java/Controller/AdminRestaurantAddController.java
@@ -0,0 +1,25 @@
+package Controller;
+
+import java.io.IOException;
+
+import DAOs.AdminDAOs;
+import DAOs.SellerProductDAOs;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+
+@WebServlet(name = "AdminRestaurantAddController", urlPatterns = {"/Admin/Restaurant/Add"})
+public class AdminRestaurantAddController extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        HttpSession session = req.getSession();
+        int  restaurantID = Integer.parseInt(req.getParameter("restaurantID"));
+        String restaurantName = req.getParameter("restaurantName");
+        String restaurantAddress = req.getParameter("restaurantAddress");
+        String restaurantDescription = req.getParameter("restaurantDescription");
+        String restaurantImage = req.getParameter("restaurantImage");
+
+        AdminDAOs adminDAOs = new AdminDAOs();
+        adminDAOs.updateRestaurant(restaurantName, restaurantAddress, restaurantDescription, restaurantImage, restaurantID);
+    }
+}
