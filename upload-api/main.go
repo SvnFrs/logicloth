0dfd4b358b87c2e7fbcbecbdecf22e4d91cef670
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2024-03-13 14:00:32 +0700

    Update path for upload api.

--- upload-api/main.go
+++ upload-api/main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/uploadFood", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 
 		file, header, err := r.FormFile("file")
@@ -24,6 +24,42 @@ func main() {
 		}(file)
 
 		out, err := os.Create("../src/main/webapp/images/foods/" + header.Filename)
+		if err != nil {
+			http.Error(w, "Failed to create file", http.StatusInternalServerError)
+			return
+		}
+		defer out.Close()
+
+		_, err = io.Copy(out, file)
+		if err != nil {
+			http.Error(w, "Failed to save file", http.StatusInternalServerError)
+			return
+		}
+
+		// w.Write([]byte("File uploaded successfully"))
+		// return uploaded file path
+		_, err = w.Write([]byte("./images/foods/" + header.Filename))
+		if err != nil {
+			return
+		}
+	})
+
+	http.HandleFunc("/uploadRestaurant", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		file, header, err := r.FormFile("file")
+		if err != nil {
+			http.Error(w, "Failed to read file", http.StatusBadRequest)
+			return
+		}
+		defer func(file multipart.File) {
+			err := file.Close()
+			if err != nil {
+
+			}
+		}(file)
+
+		out, err := os.Create("../src/main/webapp/images/restaurants/" + header.Filename)
 		if err != nil {
 			http.Error(w, "Failed to create file", http.StatusInternalServerError)
 			return
