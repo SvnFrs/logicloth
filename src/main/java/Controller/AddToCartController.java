ae8b41410e15d75e8e23edf6af0bcdf0b29d02ce
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2024-03-18 05:34:44 +0700

    Fix  bug

--- src/main/java/Controller/AddToCartController.java
+++ src/main/java/Controller/AddToCartController.java
@@ -20,20 +20,24 @@ public class AddToCartController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
-        CartDAOs cartDAOs = new CartDAOs();
-        boolean isInCart = cartDAOs.isProductInCart((int) session.getAttribute("userID"), Integer.parseInt(req.getParameter("productID")));
-        int productID = Integer.parseInt(req.getParameter("productID"));
-        int userID = (int) session.getAttribute("userID");
-        int quantity = Integer.parseInt(req.getParameter("quantity"));
-        if (isInCart) {
-            cartDAOs.addQuantity(userID, productID);
+        if (session.getAttribute("userID") == null || session.getAttribute("userID").equals("") || session.getAttribute("userCookie") == null) {
+            resp.sendRedirect(req.getContextPath() + "/Login");
         } else {
-            cartDAOs.addToCart(userID, productID, quantity);
-        }
-        // Redirect to restaurant page
-        resp.sendRedirect(req.getContextPath() + "/Restaurant?restaurantID=" + req.getParameter("restaurantID"));
+            CartDAOs cartDAOs = new CartDAOs();
+            boolean isInCart = cartDAOs.isProductInCart((int) session.getAttribute("userID"), Integer.parseInt(req.getParameter("productID")));
+            int productID = Integer.parseInt(req.getParameter("productID"));
+            int userID = (int) session.getAttribute("userID");
+            int quantity = Integer.parseInt(req.getParameter("quantity"));
+            if (isInCart) {
+                cartDAOs.addQuantity(userID, productID);
+            } else {
+                cartDAOs.addToCart(userID, productID, quantity);
+            }
+            // Redirect to restaurant page
+            resp.sendRedirect(req.getContextPath() + "/Restaurant?restaurantID=" + req.getParameter("restaurantID"));
 //        RequestDispatcher dispatcher = req.getRequestDispatcher("/User");
 //        dispatcher.forward(req, resp);
+        }
     }
 
     @Override
