27044c0c4bdb91a6c727208af828aed340f37164
“SvnFrs” <thaidvqce170410@fpt.edu.vn>
2024-03-17 18:18:34 +0700

    Update SQL Scripts.

--- init/postgres.sql
+++ init/postgres.sql
@@ -288,3 +288,4 @@ VALUES
 
 INSERT INTO OrderStatus (status_name, status_role) VALUES ('Đang xử lý', 'seller'), ('Đang chuẩn bị', 'seller'), ('Đang giao hàng', 'seller'), ('Đã nhận hàng', 'user'), ('Đã hoàn thành', 'user'), ('Đã hủy', 'user'), ('Đã từ chối', 'seller');
 UPDATE products p SET quantity = 10 WHERE p.quantity < 10;
+CREATE EXTENSION IF NOT EXISTS unaccent;
