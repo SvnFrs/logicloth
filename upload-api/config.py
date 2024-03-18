80e1f981ae42fed7e21588cfbcfbdddb79a8ea51
Kang The Variants <78076796+khengyun@users.noreply.github.com>
2024-03-19 02:57:24 +0700

    Create config.py

---
+++ upload-api/config.py
@@ -0,0 +1,10 @@
+# config.py
+db_config = {
+    'host': 'postgres',
+    'port': '5432',
+    'user': 'postgres',
+    'password': 'postgres',
+    'dbname': 'DaCFood'
+}
+
+CLIENT_ID = '520426773417-jkl1slr0qquv9vrujs83crfmf8f8nnlg.apps.googleusercontent.com'
